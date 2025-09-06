2b936daee5a7da24246a94bd06cb2a430877b41d
giovanasilverio <giovanasilveriopereiratp@gmail.com>
2025-09-06 20:44:51 -0300

    feat: add login component and routing
    - Created LoginComponent with basic template and styles.
    - Updated AppRoutingModule to route to LoginComponent.
    - Removed placeholder content from AppComponent and updated its template.
    - Deleted unused app.component.html and app.component.spec.ts files.
    - Added necessary imports and declarations in AppModule for the new LoginComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

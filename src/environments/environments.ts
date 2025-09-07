00e7eda1946ff0059ea5932215f28d4b24bebc62
giovanasilverio <giovanasilveriopereiratp@gmail.com>
2025-09-06 21:06:31 -0300

    feat: integrate Firebase authentication
    - Added @angular/fire and firebase dependencies in package.json.
    - Configured AngularFireModule and AngularFireAuthModule in app.module.ts.
    - Created environment.ts file to store Firebase configuration.

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = { 
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAtfhcaCre4Ox50SKQ2v9KP3wvam6cWMOg",
+        authDomain: "asimovies-349a7.firebaseapp.com",
+        projectId: "asimovies-349a7",
+        storageBucket: "asimovies-349a7.firebasestorage.app",
+        messagingSenderId: "68883829890",
+        appId: "1:68883829890:web:1dc2c4250a5b0ad8fb909b",
+        measurementId: "G-KVKR5V1Q96"
+    }
+}
